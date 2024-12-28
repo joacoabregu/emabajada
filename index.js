8570f4b2a3f32baaf6a0243301136434c0c6e6df
Joaquin Abregu <abregujoaquin@gmail.com>
2024-12-28 18:46:52 -0300

    feat: add biome

--- index.js
+++ index.js
@@ -1,62 +1,62 @@
-const puppeteer = require('puppeteer');
-const { sendEmail } = require('./transporter');
-const logger = require('pino')();
+const puppeteer = require("puppeteer");
+const { sendEmail } = require("./transporter");
+const logger = require("pino")();
 
 const { launch } = puppeteer;
 
-const URL = 'https://www.cgeonline.com.ar/informacion/apertura-de-citas.html';
+const URL = "https://www.cgeonline.com.ar/informacion/apertura-de-citas.html";
 
 const run = async () => {
-    logger.info('Starting scraper...');
-    const browser = await launch();
-    
-    const page = await browser.newPage();
+	logger.info("Starting scraper...");
+	const browser = await launch();
 
-  await page.goto(URL);
+	const page = await browser.newPage();
 
-  const newDate = await page.evaluate(() => {
-    const rows = document.querySelectorAll('tr');
+	await page.goto(URL);
 
-    for (let row of rows) {
-      const firstColumn = row.querySelector('td:nth-child(1)');
-      const dateColumn = row.querySelector('td:nth-child(2)');
+	const newDate = await page.evaluate(() => {
+		const rows = document.querySelectorAll("tr");
 
-      if (firstColumn && dateColumn) {
-        const firstColumnText = firstColumn.innerText;
+		for (let row of rows) {
+			const firstColumn = row.querySelector("td:nth-child(1)");
+			const dateColumn = row.querySelector("td:nth-child(2)");
 
-        if (
-          firstColumnText.includes('Pasaportes') &&
-          firstColumnText.includes('renovación y primera vez')
-        ) {
-          const dateText = dateColumn.innerText;
+			if (firstColumn && dateColumn) {
+				const firstColumnText = firstColumn.innerText;
 
-          if (dateText !== '21/11/2024') {
-            return dateText;
-          }
-        }
-      }
-    }
-    return null;
-  });
-  
-  await browser.close();
+				if (
+					firstColumnText.includes("Pasaportes") &&
+					firstColumnText.includes("renovación y primera vez")
+				) {
+					const dateText = dateColumn.innerText;
 
-  if (newDate) {
-    logger.info(`New date found: ${newDate}`);
-    logger.info('Sending email...');
-    await sendEmail(newDate);
-    logger.info('Email sent successfully.');
-  }
+					if (dateText !== "22/11/2024") {
+						return dateText;
+					}
+				}
+			}
+		}
+		return null;
+	});
 
-  logger.info('Scraper finished successfully.');
-}
+	await browser.close();
+
+	if (newDate) {
+		logger.info(`New date found: ${newDate}`);
+		logger.info("Sending email...");
+		await sendEmail(newDate);
+		logger.info("Email sent successfully.");
+	}
+
+	logger.info("Scraper finished successfully.");
+};
 
 const start = async () => {
-  try {
-      await run(); 
-  } catch (error) {
-      logger.error(error);
-  }
+	try {
+		await run();
+	} catch (error) {
+		logger.error(error);
+	}
 };
 
 start();
--- transporter.js
+++ transporter.js
@@ -1,29 +1,28 @@
-const { createTransport } = require('nodemailer');
-const dotenv = require('dotenv');
+const { createTransport } = require("nodemailer");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const transporter = createTransport({
-    host: 'smtp.sendgrid.net',
-   port: 587,
-   auth: {
-       user: "apikey",
-       pass: process.env.SENDGRID_API_KEY
-   }
-  });
+	host: "smtp.sendgrid.net",
+	port: 587,
+	auth: {
+		user: "apikey",
+		pass: process.env.SENDGRID_API_KEY,
+	},
+});
 
-  const sendEmail = async (newDate) => {
-    
-      const mailOptions = {
-        from: 'abregujoaquin@gmail.com', 
-        to: 'abregujoaquin@gmail.com',
-        subject: 'NUEVA FECHA EMBAJADA',
-        text: `La nueva fecha es ${newDate}`,
-      };
-  
-      await transporter.sendMail(mailOptions);
-  };
+const sendEmail = async (newDate) => {
+	const mailOptions = {
+		from: "abregujoaquin@gmail.com",
+		to: "abregujoaquin@gmail.com",
+		subject: "NUEVA FECHA EMBAJADA",
+		text: `La nueva fecha es ${newDate}`,
+	};
 
-  module.exports = {
-    sendEmail,
-  };
+	await transporter.sendMail(mailOptions);
+};
+
+module.exports = {
+	sendEmail,
+};
